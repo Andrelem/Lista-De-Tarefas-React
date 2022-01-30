3834daef6e11e957eacb184a5a487fb3d29088ef
Andrelem <progameslepe@gmail.com>
2022-01-30 20:45:41 -0300

    'main'

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import * as C from './App.styles';
-
+import { Item } from './types/item';
 
 const App = () => {
-  const [list,setList] = useState([]);
-  
+  const [list, setList] = useState<Item[]>([
+    { id:1, name:'comprar pao',done:false},
+    { id:2, name:'comprar bolo',done:false}
+  ]);
+
 
   return (
       <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
+          
+          
+        
         </C.Area>
       </C.Container>
   );
